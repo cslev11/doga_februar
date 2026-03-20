c4c13a47fe54cbd6eecb5c84f074996160daabbb
Cserna Levente Ferenc <cserna.levente@bolyai.local>
2026-03-20 08:00:56 +0100

    kerdesek

---
+++ kerdesek.js
@@ -0,0 +1,35 @@
+//Mi az osztály?
+
+
+//Mi a példányosítás?
+//Amikor egy osztály segítségével meghívunk egy új példányt
+
+//Mi a kapcsolat az osztály és a példány között?
+
+//Mi a higher order function?
+//Egy metódus, ami függvényt kap be paraméterként, vagy függvénnyel tér vissza
+
+//Mit nevezünk callbacknek?
+//Egy metódus, amit egy függvény paraméterenként adunk meg, amit később meghívunk
+
+
+
+//Mit ír ki az alábbi program?
+//hibás mert
+
+const result = foo("1", "2,", (szam1, szam2) => {return szam1+szam2});
+
+//mert nem kell itt a return
+
+
+//short hand property
+
+
+
+//destructing
+
+
+//a,b > 10
+
+
+//két szám 0 és 10 között van
